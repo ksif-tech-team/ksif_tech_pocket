6d7422f1a28f79db158d4dd5453c708c3950cbdd
KwangHyoun <unipastime@naver.com>
2019-02-11 17:31:13 +0900

    Merge branch 'master' of https://github.com/ksif-tech-team/ksif_tech_pocket into HEAD
    # Conflicts:
    #	pocket.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 import unittest
 from pocket import Pocket
 from datetime import datetime
@@ -17,3 +18,15 @@ class PocketTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+'''
+
+# ---------- CHECK DATA PACKAGE ----------
+
+from data import price
+
+print(price.price_slow('삼성전자', start = '2019-01-01'))
+print(price.price_slow(['삼성전자', 'SK하이닉스'], start = '2019-01-01'))
+
+data = print(price.price_slow(['삼성전자', 'SK하이닉스'], start = '2019-01-01'))
+
+
